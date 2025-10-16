2f0c4d6936b1bb8751700483820de78e2eec0d81
kamiy2743 <kamiy2743@gmail.com>
2025-10-16 19:58:33 +0900

    ApplicationQuitter追加

---
+++ Assets/GameKit/Application/Scripts/ApplicationLifetimeScope.cs
@@ -0,0 +1,13 @@
+﻿using GameKit.DependencyInjection;
+using VContainer;
+
+namespace GameKit.Application
+{
+    public sealed class ApplicationLifetimeScope : BaseLifetimeScopeRegistration<ApplicationLifetimeScope>
+    {
+        public override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<ApplicationQuitter>(Lifetime.Singleton);
+        }
+    }
+}
---
+++ Assets/GameKit/Application/Scripts/ApplicationQuitter.cs
@@ -0,0 +1,14 @@
+﻿namespace GameKit.Application
+{
+    public sealed class ApplicationQuitter
+    {
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
