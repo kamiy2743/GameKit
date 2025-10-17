e152fe93a7211a6aa5a009b072653171e99a88d1
kamiy2743 <kamiy2743@gmail.com>
2025-10-17 22:14:06 +0900

    RootLifetimeScopeで自動でRegisterする

---
+++ Assets/GameKit/DependencyInjection/Scripts/LifetimeScopeRegistrationGatherer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace GameKit.DependencyInjection.Root
+{
+    public sealed class LifetimeScopeRegistrationGatherer
+    {
+        public static IEnumerable<ILifetimeScopeRegistration> Get()
+        {
+            var instances = new List<ILifetimeScopeRegistration>();
+            var seen = new HashSet<Type>();
+
+            foreach (var type in GetTypes())
+            {
+                if (!seen.Add(type))
+                {
+                    continue;
+                }
+
+                if (!IsBaseLifetimeScopeRegistration(type))
+                {
+                    continue;
+                }
+
+                if (Activator.CreateInstance(type) is ILifetimeScopeRegistration instance)
+                {
+                    instances.Add(instance);
+                }
+            }
+
+            return instances;
+        }
+
+        static IEnumerable<Type> GetTypes()
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in GetTypesFromAssembly(assembly))
+                {
+                    yield return type;
+                }
+            }
+        }
+        
+        static IEnumerable<Type> GetTypesFromAssembly(Assembly assembly)
+        {
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(type => type != null);
+            }
+        }
+
+        static bool IsBaseLifetimeScopeRegistration(Type type)
+        {
+            if (type.IsAbstract)
+            {
+                return false;
+            }
+
+            for (var current = type; current != null; current = current.BaseType)
+            {
+                if (!current.IsGenericType)
+                {
+                    continue;
+                }
+                if (current.GetGenericTypeDefinition() == typeof(BaseLifetimeScopeRegistration<>))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- Assets/GameKit/DependencyInjection/Scripts/Root/RootLifetimeScope.cs
+++ Assets/GameKit/DependencyInjection/Scripts/Root/RootLifetimeScope.cs
@@ -8,9 +8,13 @@ namespace GameKit.DependencyInjection.Root
     public sealed class RootLifetimeScope : LifetimeScope
     {
         [SerializeField] List<BaseMBLifetimeScopeRegistration> registrations;
-        
+
         protected override void Configure(IContainerBuilder builder)
         {
+            foreach (var registration in LifetimeScopeRegistrationGatherer.Get())
+            {
+                registration.Configure(builder);
+            }
             foreach (var registration in registrations)
             {
                 registration.Configure(builder);
