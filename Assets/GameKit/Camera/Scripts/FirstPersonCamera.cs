6c316a150facadd73084de6ea1398b0427c5722e
kamiy2743 <kamiy2743@gmail.com>
2025-10-24 11:15:24 +0900

    FirstPersonCamera実装

---
+++ Assets/GameKit/Camera/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,62 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace GameKit.Camera
+{
+    public sealed class FirstPersonCamera : MonoBehaviour
+    {
+        [SerializeField] float accelTime;
+        [SerializeField] float decelTime;
+        [SerializeField] float baseSensitivity;
+        [SerializeField] Vector2 pitchLimits = new(-80f, 80f);
+
+        Vector2 input;
+        Vector2 angularVelocity;
+        float yaw;
+        float pitch;
+
+        public void SetLookInput(Vector2 input)
+        {
+            this.input = input;
+        }
+
+        void Update()
+        {
+            var dt = Time.deltaTime;
+            UpdateAngularVelocity(dt);
+
+            yaw += angularVelocity.x * dt;
+            pitch += -angularVelocity.y * dt;
+
+            yaw = WrapAngle(yaw);
+            pitch = Mathf.Clamp(pitch, pitchLimits.x, pitchLimits.y);
+
+            transform.rotation = Quaternion.Euler(pitch, yaw, 0);
+        }
+
+        void UpdateAngularVelocity(float dt)
+        {
+            var desiredVelocity = input * baseSensitivity;
+
+            angularVelocity = new Vector2(
+                UpdateAxisVelocity(angularVelocity.x, desiredVelocity.x, dt),
+                UpdateAxisVelocity(angularVelocity.y, desiredVelocity.y, dt));
+        }
+
+        float UpdateAxisVelocity(float current, float desired, float dt)
+        {
+            var dampTime = Mathf.Abs(desired) > Mathf.Abs(current) ? accelTime : decelTime;
+            return current + Damper.Damp(desired - current, dampTime, dt);
+        }
+
+        static float WrapAngle(float angle)
+        {
+            angle %= 360f;
+            if (angle < 0f)
+            {
+                angle += 360f;
+            }
+            return angle;
+        }
+    }
+}
