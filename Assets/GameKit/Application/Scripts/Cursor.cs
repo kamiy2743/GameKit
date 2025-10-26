1ea58ccf571627a6a0ba4948432fa754d37525c7
kamiy2743 <kamiy2743@gmail.com>
2025-10-26 11:04:56 +0900

    Cursorの制御

---
+++ Assets/GameKit/Application/Scripts/Cursor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GameKit.Application
+{
+    public sealed class Cursor
+    {
+        public static void SetVisible(bool visible)
+        {
+            UnityEngine.Cursor.visible = visible;
+            UnityEngine.Cursor.lockState = visible ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
--- Assets/GameKit/UIFramework/Scripts/Page/PageContainer.cs
+++ Assets/GameKit/UIFramework/Scripts/Page/PageContainer.cs
@@ -1,11 +1,18 @@
 ﻿using System.Threading;
 using Cysharp.Threading.Tasks;
+using R3;
 
 namespace GameKit.UIFramework.Page
 {
     public sealed class PageContainer
     {
         readonly UnityScreenNavigator.Runtime.Core.Page.PageContainer pageContainer;
+
+        readonly Subject<Unit> firstPageOpened = new();
+        public Observable<Unit> FirstPageOpened => firstPageOpened;
+        
+        readonly Subject<Unit> lastPageClosed = new();
+        public Observable<Unit> LastPageClosed => lastPageClosed;
 
         public PageContainer(UnityScreenNavigator.Runtime.Core.Page.PageContainer pageContainer)
         {
@@ -14,26 +21,44 @@ namespace GameKit.UIFramework.Page
 
         public async UniTask PushAsync(PageName pageName, CancellationToken ct)
         {
-            ct.ThrowIfCancellationRequested();
-            await pageContainer.Push(pageName.ResourceKey, true);
+            await PushAsync(pageName, true, ct);
         }
         
         public async UniTask PushWithoutAnimationAsync(PageName pageName, CancellationToken ct)
         {
+            await PushAsync(pageName, false, ct);
+        }
+        
+        async UniTask PushAsync(PageName pageName, bool playAnimation, CancellationToken ct)
+        {
             ct.ThrowIfCancellationRequested();
-            await pageContainer.Push(pageName.ResourceKey, false);
+            await pageContainer.Push(pageName.ResourceKey, playAnimation);
+            if (pageContainer.Pages.Count == 1)
+            {
+                firstPageOpened.OnNext(Unit.Default);
+            }
         }
         
         public async UniTask PopAsync(CancellationToken ct)
         {
             ct.ThrowIfCancellationRequested();
+            var beforePageCount = pageContainer.Pages.Count;
             await pageContainer.Pop(true);
+            if (beforePageCount == 1 && pageContainer.Pages.Count == 0)
+            {
+                lastPageClosed.OnNext(Unit.Default);
+            }
         }
         
         public async UniTask PopAllAsync(CancellationToken ct)
         {
             ct.ThrowIfCancellationRequested();
+            var beforePageCount = pageContainer.Pages.Count;
             await pageContainer.Pop(true, pageContainer.Pages.Count);
+            if (beforePageCount > 0)
+            {
+                lastPageClosed.OnNext(Unit.Default);
+            }
         }
     }
 }
