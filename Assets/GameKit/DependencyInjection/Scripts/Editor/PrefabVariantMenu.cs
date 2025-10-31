ff9e17794b439234b865ac93daad19c4daa06f3a
kamiy2743 <kamiy2743@gmail.com>
2025-10-31 22:20:44 +0900

    RootLifetimeScopeを作成できるように

---
+++ Assets/GameKit/DependencyInjection/Scripts/Editor/PrefabVariantMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using GameKit.Asset.Editor.Prefab;
+using UnityEditor;
+
+namespace GameKit.DependencyInjection.Editor
+{
+    static class PrefabVariantMenu
+    {
+        const string MenuItemRoot = "Assets/Create/GameKit/DependencyInjection/";
+        const string PackageRootPrefabPath = "DependencyInjection/Prefabs/";
+
+        [MenuItem(MenuItemRoot + "RootLifetimeScope")]
+        static void MakePageVariant()
+        {
+            PrefabVariantFactory.Make(Path.Combine(PackageRootPrefabPath, "RootLifetimeScope.prefab"));
+        }
+    }
+}
