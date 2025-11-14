decb80b872b41f2aa0aa010fd3b61646122cf54f
kamiy2743 <kamiy2743@gmail.com>
2025-11-14 18:10:20 +0900

    アニメーションなしで下のPageだけPopできるように

--- Assets/GameKit/UIFramework/Scripts/Page/PageContainer.cs
+++ Assets/GameKit/UIFramework/Scripts/Page/PageContainer.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using R3;
+using UnityEngine;
 
 namespace GameKit.UIFramework.Page
 {
@@ -41,6 +44,23 @@ namespace GameKit.UIFramework.Page
         public async UniTask PopAllAsync(CancellationToken ct)
         {
             await PopAsync(popCount: pageContainer.OrderedPagesIds.Count, ct: ct);
+        }
+
+        public async UniTask PopAllExceptTopAsync(CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+            
+            var pages = pageContainer.Pages as Dictionary<string, UnityScreenNavigator.Runtime.Core.Page.Page>;
+            var orderedPageIds = pageContainer.OrderedPagesIds as List<string>;
+            var popTargetPageIds = orderedPageIds!.Take(pageContainer.OrderedPagesIds.Count - 1).ToArray();
+            foreach (var pageId in popTargetPageIds)
+            {
+                var page = pages![pageId];
+                await page.Cleanup();
+                pages.Remove(pageId);
+                orderedPageIds!.Remove(pageId);
+                Object.Destroy(page.gameObject);
+            }
         }
 
         public BasePage? GetActivePage()
