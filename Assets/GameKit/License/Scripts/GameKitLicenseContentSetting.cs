a336761b0914ba2889ada167e2f2bc179e4c1677
kamiy2743 <kamiy2743@gmail.com>
2025-11-16 16:53:14 +0900

    GameKitのライセンスはGameKitで管理する

---
+++ Assets/GameKit/License/Scripts/GameKitLicenseContentSetting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GameKit.License
+{
+    [CreateAssetMenu(fileName = "GameKitLicenseContentSetting", menuName = "GameKit/License/GameKitLicenseContentSetting")]
+    public sealed class GameKitLicenseContentSetting : ScriptableObject, ILicenseContentValueProvider
+    {
+        [SerializeField] LicenseContent[] licenseContents;
+        
+        IReadOnlyList<LicenseContentValue> ILicenseContentValueProvider.Get()
+        {
+            return licenseContents.Select(x => new LicenseContentValue(x.Name, x.Body)).ToArray();
+        }
+        
+        [Serializable]
+        public sealed record LicenseContent
+        {
+            [SerializeField] string name;
+            [SerializeField][TextArea] string body;
+            
+            public string Name => name;
+            public string Body => body;
+        }
+    }
+}
