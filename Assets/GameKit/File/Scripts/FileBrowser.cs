d9cc507c4e406505baba34dad800dcf5d78d3ff6
kamiy2743 <kamiy2743@gmail.com>
2025-11-16 15:56:43 +0900

    LocalizedString追加

--- Assets/GameKit/File/Scripts/FileBrowser.cs
+++ Assets/GameKit/File/Scripts/FileBrowser.cs
@@ -1,20 +1,27 @@
 ﻿using System.Threading;
 using Cysharp.Threading.Tasks;
+using GameKit.Localization;
 using SFB;
 
 namespace GameKit.File
 {
     public sealed class FileBrowser
     {
-        public async UniTask<FilePath> SelectFileAsync(string dialogTitle, string extension, CancellationToken ct)
+        public async UniTask<FilePath> SelectFileAsync(LocalizedString dialogTitle, string extension, CancellationToken ct)
         {
             ct.ThrowIfCancellationRequested();
             
-            var paths = StandaloneFileBrowser.OpenFilePanel(dialogTitle, "", extension, false);
+            var paths = StandaloneFileBrowser.OpenFilePanel(
+                dialogTitle.GetValue(),
+                "",
+                extension,
+                false
+            );
             if (paths.Length == 0 || string.IsNullOrEmpty(paths[0]))
             {
                 throw new SelectFileCanceledException();
             }
+
             return new FilePath(paths[0]);
         }
     }
---
+++ Assets/GameKit/Localization/Scripts/LocalizedString.cs
@@ -0,0 +1,10 @@
+﻿namespace GameKit.Localization
+{
+    public sealed record LocalizedString
+    {
+        public string GetValue()
+        {
+            return string.Empty;
+        }
+    }
+}
