01f02fc13a49fd5bcd91504f3a508a03589abdf6
kamiy2743 <kamiy2743@gmail.com>
2025-11-23 23:49:03 +0900

    IndexOf追加

---
+++ Assets/GameKit/Collection/Scripts/IndexOfExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameKit.Collection
+{
+    public static class IndexOfExtension
+    {
+        public static int IndexOf<T>(this IEnumerable<T> collection, T target) where T : IEquatable<T>
+        {
+            var index = 0;
+
+            foreach (var value in collection)
+            {
+                if (value.Equals(target))
+                {
+                    return index;
+                }
+                index++;
+            }
+
+            return -1;
+        }
+    }
+}
