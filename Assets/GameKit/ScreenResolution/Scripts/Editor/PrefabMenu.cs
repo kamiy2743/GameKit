5ed67355802ed84587afe2fb9eb9ba1c7460a7bd
kamiy2743 <kamiy2743@gmail.com>
2025-11-24 11:44:38 +0900

    ScreenResolutionSettingを配置

---
+++ Assets/GameKit/ScreenResolution/Scripts/Editor/PrefabMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using GameKit.Asset.Editor.Prefab;
+using UnityEditor;
+
+namespace GameKit.ScreenResolution.Editor
+{
+    static class PrefabMenu
+    {
+        const string HierarchyPrefabMenuRoot = "GameObject/GameKit/ScreenResolution/";
+        const string PackageRootPrefabPath = "ScreenResolution/Prefabs/";
+
+        [MenuItem(HierarchyPrefabMenuRoot + "ScreenResolutionSetting")]
+        static void MakeScreenResolutionSetting()
+        {
+            HierarchyPrefabFactory.Make(Path.Combine(PackageRootPrefabPath, "ScreenResolutionSetting/ScreenResolutionSetting.prefab"));
+        }
+    }
+}
