65518e6a49156d82e5c6b70ea3efcaade3c1bb37
kamiy2743 <kamiy2743@gmail.com>
2025-12-21 14:00:09 +0900

    未作成、更新有のチャンクだけメッシュを生成する

---
+++ Assets/GameKit/Collection/Scripts/NativeListExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+
+namespace GameKit.Collection
+{
+    public static class NativeListExtension
+    {
+        public static IEnumerable<T> AsEnumerable<T>(this NativeList<T> list) where T : unmanaged
+        {
+            return new NativeListEnumerable<T>(list);
+        }
+        
+        readonly struct NativeListEnumerable<T> : IEnumerable<T> where T : unmanaged
+        {
+            readonly NativeList<T> list;
+
+            public NativeListEnumerable(NativeList<T> list)
+            {
+                this.list = list;
+            }
+
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+        }
+    }
+}
