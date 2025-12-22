c74d55a46c97afb1149aabc7ead248f30c4d0717
kamiy2743 <kamiy2743@gmail.com>
2025-12-22 18:18:55 +0900

    Nest対応

---
+++ Assets/GameKit/Collection/Scripts/NestableReadOnlyNativeArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace GameKit.Collection
+{
+    public readonly unsafe struct NestableReadOnlyNativeArray<T> where T : unmanaged
+    {
+        readonly T* ptr;
+
+        public int Length { get; }
+        
+        public ref T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return ref ptr[index];
+            }
+        }
+
+        public NestableReadOnlyNativeArray(NativeArray<T> source)
+        {
+            ptr = (T*)source.GetUnsafeReadOnlyPtr();
+            Length = source.Length;
+        }
+    }
+}
