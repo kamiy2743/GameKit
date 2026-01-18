308200bbf465835be0d23a5246c2dfdb342adef0
kamiy2743 <kamiy2743@gmail.com>
2026-01-18 11:55:24 +0900

    BurstHashCode追加

---
+++ Assets/GameKit/Burst/Scripts/BurstHashCode.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace GameKit.Burst
+{
+    public static class BurstHashCode
+    {
+        const int Multiplier = 397;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine(int value1)
+        {
+            return value1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine(int value1, int value2)
+        {
+            unchecked
+            {
+                var hash = value1;
+                hash = (hash * Multiplier) ^ value2;
+                return hash;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine(int value1, int value2, int value3)
+        {
+            unchecked
+            {
+                var hash = Combine(value1, value2);
+                hash = (hash * Multiplier) ^ value3;
+                return hash;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Combine(int value1, int value2, int value3, int value4)
+        {
+            unchecked
+            {
+                var hash = Combine(value1, value2, value3);
+                hash = (hash * Multiplier) ^ value4;
+                return hash;
+            }
+        }
+    }
+}
