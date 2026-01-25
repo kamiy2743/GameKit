5b19fd5ea4f4599c1b24b99515972b0d28a83208
kamiy2743 <kamiy2743@gmail.com>
2026-01-25 16:45:28 +0900

    job完了を待機できるように

---
+++ Assets/GameKit/JobSystem/Scripts/JobExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using Unity.Jobs;
+
+namespace GameKit.JobSystem
+{
+    public static class JobExtension
+    {
+        public static async UniTask ScheduleAsync<T>(
+            this T job,
+            JobHandle dependsOn = default,
+            CancellationToken ct = default
+        )
+            where T : struct, IJob
+        {
+            var handle = job.Schedule(dependsOn);
+            await handle.WaitForAsync(ct);
+        }
+        
+        public static async UniTask ScheduleParallelAsync<T>(
+            this T job,
+            int arrayLength,
+            int innerLoopBatchCount,
+            JobHandle dependsOn = default,
+            CancellationToken ct = default
+        )
+            where T : struct, IJobFor
+        {
+            var handle = job.ScheduleParallel(arrayLength, innerLoopBatchCount, dependsOn);
+            await handle.WaitForAsync(ct);
+        }
+        
+        public static async UniTask WaitForAsync(this JobHandle handle, CancellationToken ct)
+        {
+            try
+            {
+                await UniTask.WaitUntil(() => handle.IsCompleted, cancellationToken: ct);
+            }
+            finally
+            {
+                handle.Complete();
+            }
+        }
+    }
+}
